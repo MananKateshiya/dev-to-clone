fb05d03b45ce442558c546aa8cdf9c08935124cf
MananKateshiya <manankateshiya@gmail.com>
2023-09-07 13:37:08 +0530

    Minor bug fixes to LeftSidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -280,7 +280,7 @@ function LeftSidebar() {
             height="24"
             viewBox="0 0 24 24"
             role="img"
-            className="crayons-icon rounded-md p-2 h-10 w-10 hover:bg-hoverAccent hover:fill-blue-900"
+            className="cursor-pointer rounded-md p-2 h-10 w-10 hover:bg-hoverAccent hover:fill-blue-900"
           >
             <path d="M12 1l9.5 5.5v11L12 23l-9.5-5.5v-11L12 1zm0 2.311L4.5 7.653v8.694l7.5 4.342 7.5-4.342V7.653L12 3.311zM12 16a4 4 0 110-8 4 4 0 010 8zm0-2a2 2 0 100-4 2 2 0 000 4z"></path>
           </svg>
@@ -290,9 +290,9 @@ function LeftSidebar() {
             <Link
               key={tags.tag}
               href={`/t/${tags.tag}`}
-              className=" flex items-center px-2 py-1.5 bg-transparent focus:focusRing focus:bg-hoverAccent hover:bg-hoverAccent hover:text-blue-900 hover:underline focus:underline rounded-md"
+              className=" flex items-center px-2.5 py-1.5 bg-transparent focus:focusRing focus:bg-hoverAccent hover:bg-hoverAccent hover:text-blue-900 hover:underline focus:underline rounded-md"
             >
-              <span className="text-md py-1 focus:focusRing focus:bg-hoverAccent ">
+              <span className="text-md py-0.5 focus:focusRing focus:bg-hoverAccent ">
                 #{tags.tag}
               </span>
             </Link>
