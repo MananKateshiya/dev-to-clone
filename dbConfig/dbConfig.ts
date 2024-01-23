29ded84d2e893a8ea139c622c6633573f0267cc6
MananKateshiya <manankateshiya@gmail.com>
2024-01-23 14:54:59 +0530

    chore: updated mongoose.connect for vercel

--- dbConfig/dbConfig.ts
+++ dbConfig/dbConfig.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export async function connect() {
   try {
-    mongoose.connect(process.env.DATABASE_URL!);
+    mongoose.connect(`${process.env.DATABASE_URL!}`);
     const connection = mongoose.connection;
     connection.on('connected',()=>{
         console.log("DATABASE IS CONNECTED")
