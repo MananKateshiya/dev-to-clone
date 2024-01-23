cf726466459d926db8da2f037f08eea37adca211
MananKateshiya <manankateshiya@gmail.com>
2024-01-23 16:40:25 +0530

    feat: Added middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/enter" || path === "/enter/signup";
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/enter", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/enter/:path*",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
